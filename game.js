de333440c60c8898e50f4d1a866b863ce9845ddd
akshithareddi3 <akshithareddynimma@gmail.com>
2020-08-21 23:05:00 +0530

    commit all

---
+++ game.js
@@ -0,0 +1,36 @@
+const $submitButton = document.getElementById("submit");
+const $message = document.getElementById("message");
+const $lives = document.getElementById("lives");
+
+var guessNumber = Math.round(Math.random()*100);
+var lives = 10;
+var message ;
+
+$submitButton.onclick = () =>
+{
+    let userInput = document.getElementById("number-input").value ;
+    lives--;
+    if(userInput == guessNumber)
+    {
+        location.href="./win.html";
+
+    }
+    else if(lives== 0)
+    {
+        location.href="./lose.html";
+    
+    }
+    else if(userInput > guessNumber)
+    {
+        message= `oops ! Your guess is too high. you have ${lives} lives remaining.`;
+    }
+    else if(userInput < guessNumber)
+    {
+        message= `oops ! Your guess is too low. you have ${lives} lives remaining.`;
+    }
+
+    $message.style.display="inherit";
+    $message.innerHTML =message;
+    $lives.innerHTML=lives ;
+
+}
